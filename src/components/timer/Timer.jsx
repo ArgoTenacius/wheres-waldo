451f88cc41386a53499f551bb6d55f9fd9c4eb5b
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-05-04 18:41:46 -0300

    Clean code.

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,22 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { formatTime } from '../../util/util';
 
-const Timer = (timer) => {
-
+const Timer = ({timer, gameOver}) => {
     let interval;
     useEffect(() => {
+      if(!gameOver){
         interval = setInterval(() => {
-            timer.timer.setTime((second) => second + 1);
+            timer.setTime((second) => second + 1);
         }, 1000);
+      }else{
+        clearInterval(interval);
+      }
 
-        return () => {
-            clearInterval(interval);
-        }
+      return () => {
+          clearInterval(interval);
+      }
     })
 
   return (
-    <h4>{formatTime(timer.timer.time)}</h4>
+    <h4>{formatTime(timer.time)}</h4>
   )
 }
 
