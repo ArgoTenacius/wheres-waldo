830c53ee18b0b1b180a4bcf4fd5f849a9b219f0c
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-05-06 14:46:56 -0300

    Rank list sort added.

--- src/components/ranks/Ranks.jsx
+++ src/components/ranks/Ranks.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './ranks.css';
 import { AiOutlineMenu } from 'react-icons/ai'
+import { formatTime } from '../../util/util';
 
 const Ranks = ({rankList, openRank}) => {
 
@@ -15,16 +16,22 @@ const Ranks = ({rankList, openRank}) => {
           document.removeEventListener('click', checkClick);
         }
     })
+
+    const sortByTime = (x, y) => {
+        return x.time - y.time;
+    }
+
+    rankList.sort(sortByTime)
     
     return (
     <section className='rank'>
         <AiOutlineMenu onClick={() => openRank()} className='navbar__icon'/>
-        {rankList.map((player) => (
+        {rankList.map((player, index) => (
             <section key={player.id}>
                 <div className='rank__player'>
-                    <h1 className='rank__player-place'>1.</h1>
+                    <h1 className='rank__player-place'>{index + 1}.</h1>
                     <h2 className='rank__player-name'>{player.name}</h2>
-                    <h2 className='rank__player-time'>{player.time}s</h2>
+                    <h2 className='rank__player-time'>{formatTime(player.time)}s</h2>
                 </div>
                 <hr />
             </section>
